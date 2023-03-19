e9733913b69a494cb0b59dd5c901de1997c77fa2
BruceXinXin <1592500029xpx@gmail.com>
2023-03-19 16:28:36 -0400

    Updates for PickingHistoryCollector.

--- cs561/PickingHistoryCollector.cc
+++ cs561/PickingHistoryCollector.cc
@@ -1,17 +1,31 @@
+#include <fstream>
+
 #include "PickingHistoryCollector.h"
 
+using std::endl;
 
 void PickingHistoryCollector::AddPickingFile(const std::vector<ROCKSDB_NAMESPACE::Fsize> &temp, int index) {
 //    std::string serialized_tmp = serialize_Fsize_vec(temp);
 //    m_history[serialized_tmp].insert(index);
-    m_history[temp].insert(index);
+
+    static std::hash<std::vector<ROCKSDB_NAMESPACE::Fsize>> hasher;
+    m_history[hasher(temp)].insert(index);
 }
 
+void PickingHistoryCollector::AddPickingFile(size_t hash_value, int index) {
+    m_history[hash_value].insert(index);
+}
 
 bool PickingHistoryCollector::IsInHistory(const std::vector<ROCKSDB_NAMESPACE::Fsize> &temp, int index) {
 //    std::string serialized_tmp = serialize_Fsize_vec(temp);
 //    return m_history[serialized_tmp].count(index);
-    return m_history[temp].count(index);
+
+    static std::hash<std::vector<ROCKSDB_NAMESPACE::Fsize>> hasher;
+    return m_history[hasher(temp)].count(index);
+}
+
+bool PickingHistoryCollector::IsInHistory(size_t hash_value, int index) {
+    return m_history[hash_value].count(index);
 }
 
 std::string PickingHistoryCollector::serialize_Fsize_vec(const std::vector<ROCKSDB_NAMESPACE::Fsize> &temp) {
@@ -36,4 +50,40 @@ std::string PickingHistoryCollector::serialize_Fsize_vec(const std::vector<ROCKS
         res.push_back('A');
     }
     return res;
+}
+
+void PickingHistoryCollector::recover_from_file() {
+    std::fstream f(DUMP_FILENAME);
+    m_history.clear();
+
+    std::string s;
+    while (!f.eof()) {
+        size_t hash_value;
+        size_t n;
+        int idx;
+        std::set<int> indexes;
+
+        f >> hash_value >> n;
+        for (int i = 0; i < n; ++ i) {
+            f >> idx;
+            indexes.insert(idx);
+        }
+
+        m_history[hash_value] = std::move(indexes);
+    }
+    f.close();
+}
+
+void PickingHistoryCollector::dump_to_file() {
+    std::fstream f(DUMP_FILENAME);
+    f.clear();
+
+    for (const auto& [hash_value, indexes]: m_history) {
+        f << hash_value << endl;
+        f << indexes.size() << endl;
+        for (int idx: indexes)
+            f << idx << endl;
+    }
+
+    f.close();
 }
--- cs561/PickingHistoryCollector.h
+++ cs561/PickingHistoryCollector.h
@@ -14,15 +14,15 @@ class PickingHistoryCollector;
 
 namespace std {
     template<>
-    struct std::hash<std::vector<ROCKSDB_NAMESPACE::Fsize>> {
+    struct hash<std::vector<ROCKSDB_NAMESPACE::Fsize>> {
         inline size_t operator()(const std::vector<ROCKSDB_NAMESPACE::Fsize>& temp) const {
             static FileSerializer serializer;
-            static std::hash<std::string> hasher;
+            static hash<std::string> hasher;
 
             size_t res = 0;
 
             for (const auto& t: temp)
-                res ^= hasher(serializer.Serialize(t));
+                res ^= hasher(serializer.Serialize(t.file));
 
             return res;
         }
@@ -35,7 +35,9 @@ private:
     // serialized FileMetaData(s) -> chose indexes
 //    std::unordered_map<std::string, std::set<int>> m_history;
     // hashed vector -> chose indexes
-    std::unordered_map<std::vector<ROCKSDB_NAMESPACE::Fsize>, std::set<int>> m_history;
+    std::unordered_map<size_t, std::set<int>> m_history;
+
+    inline static const std::string DUMP_FILENAME = "DumpFile";
 
 private:
     /**
@@ -49,19 +51,32 @@ private:
      */
     static std::string serialize_Fsize_vec(const std::vector<ROCKSDB_NAMESPACE::Fsize>& temp);
 
+    void recover_from_file();
+
+    void dump_to_file();
+
 public:
-    PickingHistoryCollector() = default;
-    ~PickingHistoryCollector() = default;
+    PickingHistoryCollector() {
+        recover_from_file();
+    }
+
+    ~PickingHistoryCollector() {
+        dump_to_file();
+    }
 
     // Add one selection to history (Maybe later we need to also collect the corresponding WA of this selection)
     // @param temp: the current file version
     // @param index: the index of the file that is selected
     void AddPickingFile(const std::vector<ROCKSDB_NAMESPACE::Fsize> &temp, int index);
 
+    void AddPickingFile(size_t hash_value, int index);
+
     // Check whether the index of the current file version is in the history
     // @param temp: the current file version
     // @param index: the index of the file that is going to be checked
     // @return: true if the index is in the history, false otherwise
     bool IsInHistory(const std::vector<ROCKSDB_NAMESPACE::Fsize> &temp, int index);
+
+    bool IsInHistory(size_t hash_value, int index);
 };
 
