769fb210cc7a12424d9e109757176bc800288fb4
Owen_Zhu <czhuai@connect.ust.hk>
2023-04-07 19:03:51 +0000

    Add log type

--- options/cs561/cs561_log.cc
+++ options/cs561/cs561_log.cc
@@ -9,6 +9,22 @@ void CS561Log::Log(const std::string &content, LogLevel log_level=INFO) {
 
     const auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     const auto local_time = std::localtime(&now);
-    f << std::put_time(local_time, "%Y-%m-%d %H:%M:%S") << " --- " << content << std::endl;
+    std::string log_type;
+    switch (log_level)
+    {
+    case INFO:
+        log_type = "INFO";
+        break;
+    case WARNING:
+        log_type = "INFO";
+        break;
+    case ERROR:
+        log_type = "ERROR";
+        break;
+    default:
+        log_type = "NONE";
+        break;
+    }
+    f << std::put_time(local_time, "%Y-%m-%d %H:%M:%S") << " --- " << "(" << log_type << ")" << content << std::endl;
 }
 
